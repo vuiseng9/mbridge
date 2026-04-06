09a15b0a312aaf30a7099fd0869d57b7cc552485
Vui Seng Chua <vuiseng9@gmail.com>
2026-04-06 11:44:54 -0700

    Add Makefile for perf bench, starting with 8xb200 on llama3_8b

--- scripts/performance/run_script.py
+++ scripts/performance/run_script.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import logging
 
 import torch
@@ -73,4 +74,14 @@ def main():
 
 
 if __name__ == "__main__":
+    DBG_ATTACH = False
+    if int(os.environ.get("DBG_ATTACH", "0")) == 1:
+        DBG_ATTACH = True
+        
+    if DBG_ATTACH and int(os.environ.get("RANK", "0")) == 0:
+        import debugpy
+        debugpy.listen(("127.0.0.1", 5678))
+        # optional (only when you want to pause immediately):
+        print('\n\n\n\n\n#### Waiting for debugger attach...', flush=True)
+        debugpy.wait_for_client()
     main()
